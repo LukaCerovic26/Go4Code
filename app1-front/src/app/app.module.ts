eba943ba8bb80b0d3761ac9ff1513c82f862d38d
LukaCerovic26 <144677310+LukaCerovic26@users.noreply.github.com>
2023-09-20 09:00:05 +0200

    20.09/2023 app1-front
    Login, komponente

--- app1-front/src/app/app.module.ts
+++ app1-front/src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { PostComponent } from './post/post.component';
+import { LikeComponent } from './like/like.component';
+import { CommentComponent } from './comment/comment.component';
+import { NotificationComponent } from './notification/notification.component';
+import { CreatePostComponent } from './post/create-post/create-post.component';
+import { LoginComponent } from './login/login.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    PostComponent,
+    LikeComponent,
+    CommentComponent,
+    NotificationComponent,
+    CreatePostComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
